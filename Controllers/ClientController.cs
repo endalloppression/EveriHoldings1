ca50edf13ba571a9c4bbca2c40f4e08fcda35ed0
endalloppression <endalloppression@protonmail.com>
2022-04-07 15:41:58 -0500

    Add project files.

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,38 @@
+﻿using EveriHoldings;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EveriHoldings1.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Test()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit()
+        {
+            var myClient = new ClientModel();
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "testMessage";
+                myClient.DriversLicense = "P123456789012";
+                myClient.FirstName = "Homer";
+                myClient.LastName = "Simpson";
+                myClient.DateOfBirth = DateTime.UtcNow;
+                myClient.MiddleName = "J";
+                myClient.AnnualSalary = 1.0M;
+                myClient.MonthlySpending = 0.001M;
+            }
+            return View(myClient);
+        }
+    }
+}
---
+++ Models/ClientModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EveriHoldings
+{
+	public class ClientModel
+	{
+		private string firstName;
+		[DisplayFormat(ConvertEmptyStringToNull = false),Required(ErrorMessage = "FirstName is required.")]
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = "Homer"; }
+		}
+		[DisplayFormat(ConvertEmptyStringToNull = false), Required(ErrorMessage = "Middle Name is required.")]
+		public string MiddleName { get; set; }
+		[DisplayFormat(ConvertEmptyStringToNull = false), Required(ErrorMessage = "Last Name is required.")]
+		public string LastName { get; set; }		
+		public DateTime DateOfBirth { get; set; }
+		[DisplayFormat(ConvertEmptyStringToNull = false), Required]//Do Regex pattern matching here
+		public string DriversLicense { get; set; }
+[Required]
+		public decimal AnnualSalary { get; set; }	
+[Required]
+		public decimal MonthlySpending { get; set; }
+	}
+}
